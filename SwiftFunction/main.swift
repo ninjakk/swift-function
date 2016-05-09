265d47ead2c187555c8d3f6bfe18d789d5242301
admin <admin@admins-mac-mini-236.local>
2016-05-09 16:14:00 +0700

    Initial Commit

---
+++ SwiftFunction/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftFunction
+//
+//  Created by admin on 5/9/2559 BE.
+//  Copyright © 2559 admin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
